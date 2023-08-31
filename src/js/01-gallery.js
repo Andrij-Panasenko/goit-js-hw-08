18eda6e52987278dfd7eb5515bd32804391ec04f
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-31 17:18:13 +0300

    Task 1, connect and import simplelightbox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image js-target" src="${preview}" alt="${description}"/>
+    </a>
+    </li>`;
+  })
+  .join('');
+
+galleryList.innerHTML = markup;
+
+const galleryLightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
