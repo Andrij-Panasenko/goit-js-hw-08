e243b74c84e80dcf3e326b6cc12acef596bab336
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-09-01 00:47:48 +0300

    Перша робоча версія завдання 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,63 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe)
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let isSettingTime = false;
+
+function onPlay(data) {
+    const currentTime = data.seconds;
+    
+
+    if (!isSettingTime) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+
+    }
+}
+
+const savedTimeFromStorage = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+if (savedTimeFromStorage !== null) {
+  isSettingTime = true;
+  player.setCurrentTime(savedTimeFromStorage);
+  isSettingTime = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const storage = JSON.parse(localStorage.getItem(KEY_TIME)) ?? [];
+
+
+
+// const currentTimeHold = {
+//     "videoplayer-current-time" : data,
+// }
+
+// setItem - додати значення
+// getItem - отримати значення
+// removeItem - видалити значення
+// clear - очистити все 
+// localStorage.setItem(
+//   'назва ключа' /**, JSON.stringify(значення таймера відео) */
+// );
+
+// localStorage.removeItem('назва ключа')
+// localStorage.clear()
 
