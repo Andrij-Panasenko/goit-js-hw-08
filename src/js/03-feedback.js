916a911d3d5522d7ede6af44ae1abb5c2fea427d
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-09-01 19:10:04 +0300

    MVP version

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,16 +2,36 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', onInputChange);
-
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+form.addEventListener('input', throttle(onInputChange, 500));
 
 function onInputChange(evt) {
+  evt.preventDefault();
 
+  const { email, message } = evt.currentTarget.elements;
+
+  //забираємо деструктуризовані поля форми і записуємо їх як об'єкт
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  // сетимо об'єкт в локалСторедж де ключ 'feedback-form-state' а об'єкт - {"email":"","message":""} його значення
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+//записуємо об'єкт в змінну
+const savedDataFromStorage = localStorage.getItem('feedback-form-state');
+
+//перевіряємо чи є в нас в локалСторедж дані
+if (savedDataFromStorage) {
+  //парсимо дані
+  const parsedDataFromStorage = JSON.parse(savedDataFromStorage);
+
+  // отримуємо поля форми
+  const emailField = form.querySelector('[type="email"]');
+  const textareaField = form.querySelector('[name="message"]');
+
+  //записуємо дані з локалСторедж як значення полів форми
+  emailField.value = parsedDataFromStorage.email;
+  textareaField.value = parsedDataFromStorage.message;
 }
