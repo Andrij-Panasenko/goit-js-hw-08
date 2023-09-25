61923a79155aba1c2c3481194fa5c747308734b1
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-09-25 22:27:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,21 +8,16 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-let isSettingTime = false;
 
 function onPlay(data) {
     const currentTime = data.seconds;
     
 
-    if (!isSettingTime) {
-        localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(currentTime));
-    }
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(currentTime));
 }
 
 const savedTimeFromStorage = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
 
 if (savedTimeFromStorage) {
-  isSettingTime = true;
   player.setCurrentTime(savedTimeFromStorage);
-  isSettingTime = false;
 }
